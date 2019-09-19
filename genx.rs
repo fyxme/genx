911a1e39d75e29aab8f599d15641d32aca834f97
fyx <7473283+fyxme@users.noreply.github.com>
2019-09-19 21:20:16 +1000

    Updated Speed Comparaison against Python and C equivalents and fixed speed issue

--- genx.rs
+++ genx.rs
@@ -2,78 +2,58 @@ use std::env;
 use std::fs;
 use std::path::Path;
 use std::fs::File;
-use std::io::{BufRead, BufReader, Result, BufWriter, Write};
-use std::io;
+use std::io::{BufRead, BufReader, BufWriter, Write};
 
 const MIN_ARGS: usize = 3;
 const USAGE_STR: &str = "Usage: ./genx <domain(s)> <wordlist.txt> <out_file>\n\tdomains - a string or file containing a list of domains";
 
 const GENERATED_SUBS: &str = "gen_subdomains.txt";
 
-const SUB_COMBINATIONS: &'static [&'static str] = &["{}-{}","{}{}","{}_{}"];
-
 const KEEP_INTERMEDIATE_FILE: bool = false;
-
-/*
-#[derive(Default)]
-struct ArgOptions {
-    mode: u8, // the mode to use when generating data: specific vs all up to
-    level: u8, // level of the mode: 0, 1, 2
-    domain: String, // domain name
-    in_fname: String, // input wordlist
-    //domains_fname: Option<&'static str> // input domains list
-    out_fname: Option<&'static str> // output file (Optional, will print to stdout by default)
-}
-*/
 
 fn is_file(name: &str) -> bool {
     return Path::new(name).exists();
 }
 
-fn generate_subdomains_from_wordlist(wordlist_fname: &str, out_fname: &str) -> Result<()> {
+fn generate_subdomains_from_wordlist(wordlist_fname: &str, out_fname: &str) {
     let f1 = File::open(wordlist_fname).unwrap();
     let f2 = File::open(wordlist_fname).unwrap();
 
     let mut out_writer = BufWriter::new(File::create(&Path::new(&out_fname)).unwrap());
 
     for sub1 in BufReader::new(f1).lines() {
-        let s1 = sub1?;
+        let s1 = sub1.unwrap();
         for sub2 in BufReader::new(&f2).lines() {
             // have to do it one by one since rust's format! only allows string literals (probably
             // to protect from format strings)
-            let s2 = sub2?;
-            out_writer.write(format!("{}-{}\n", s1, s2).as_bytes()).unwrap(); 
-            out_writer.write(format!("{}{}\n", s1, s2).as_bytes()).unwrap(); 
-            out_writer.write(format!("{}_{}\n", s1, s2).as_bytes()).unwrap();
+            let s2 = sub2.unwrap();
+            writeln!(out_writer, "{}-{}\n", s1, s2).unwrap();
+            writeln!(out_writer, "{}{}\n", s1, s2).unwrap();
+            writeln!(out_writer, "{}_{}\n", s1, s2).unwrap();
             /*
             if numbered {
                 for i in 0..10 {
-                    out_writer.write(format!("{}-{}\n", s1, s2).as_bytes()).unwrap(); 
-                    out_writer.write(format!("{}{}\n", s1, s2).as_bytes()).unwrap(); 
-                    out_writer.write(format!("{}_{}\n", s1, s2).as_bytes()).unwrap();
+                    writeln!(out_writer, "{}-{}\n", s1, s2)).unwrap(); 
+                    writeln!(out_writer, "{}{}\n", s1, s2)).unwrap();
+                    writeln!(out_writer, "{}_{}\n", s1, s2)).unwrap();
                 }
             }*/
         }
     }
-    Ok(())
 }
 
-fn combine_domain_wordlist(domain: &str, wordlist_fname: &str, out_writer: &mut BufWriter<File>) -> Result<()> {
-    let file = File::open(wordlist_fname).unwrap();
-
-    for line in BufReader::new(file).lines() {
-        out_writer.write(format!("{}.{}\n", line?, domain).as_bytes()).unwrap();
+fn combine_domain_wordlist(domain: &str, wordlist_fname: &str, out_writer: &mut BufWriter<File>) {
+    for line in BufReader::new(File::open(wordlist_fname).unwrap()).lines() {
+        let word = line.unwrap();
+        writeln!(out_writer, "{}.{}", word, domain);
     }
-    Ok(())
 }
 
-fn combine_list_domains_wordlist(domains_fname: &str, wordlist_fname: &str, out_writer: &mut BufWriter<File>) -> Result<()> {
-    let file = File::open(domains_fname).unwrap();
-
-    for line in BufReader::new(file).lines() {
-        combine_domain_wordlist(&line?, &wordlist_fname, out_writer);
+fn combine_list_domains_wordlist(domains_fname: &str, wordlist_fname: &str, out_writer: &mut BufWriter<File>) {
+    for domain_line in BufReader::new(File::open(domains_fname).unwrap()).lines() {
+        let domain = domain_line.unwrap();
+        combine_domain_wordlist(&domain, &wordlist_fname, out_writer);
     }
-    Ok(())
 }
 
 fn main() {
@@ -99,20 +79,6 @@ fn main() {
 
     let out_fname = args[3].clone();
     let mut out_writer = BufWriter::new(File::create(&Path::new(&out_fname)).unwrap());
-
-/*
-    let mut out_writer = BufWriter::new(match out_fname {
-            None => Box::new(io::stdout()) as Box<Write>,
-            Some(ref fname) => Box::new(File::create(&Path::new(fname)).unwrap()) as Box<Write>,
-    });
- 
-    let opts = ArgOptions { 
-        domain: args[2].clone(),
-        in_fname: args[1].clone(),
-        out_fname: Some(out_fname.clone()),
-        ..Default::default()
-    };
-*/
 
     if is_file(&args[1]) {
         combine_list_domains_wordlist(&args[1], &wordlist_fname, &mut out_writer);
